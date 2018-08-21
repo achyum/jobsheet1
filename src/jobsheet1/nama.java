81960b0a20558541534e0255854fc17606cae615
SMK TELKOM <SMK TELKOM@192.168.56.1>
2018-08-21 19:34:53 +0700

    commit keseluruhan

--- src/jobsheet1/nama.java
+++ src/jobsheet1/nama.java
@@ -14,5 +14,184 @@ public class nama {
     for (int sp = 0; sp<2; sp++){
     System.out.println("");
     }
+    for (int sp = 0; sp<5; sp++){
+    System.out.print(" ");
+    }
+    System.out.println("A");//BARIS1
+    for (int sp = 0; sp<3; sp++){
+    System.out.print(" ");
+    }
+    System.out.print("A");
+    for (int sp2 = 0; sp2<3; sp2++){
+    System.out.print(" ");
+    }
+    System.out.println("A");//BARIS2
+    for (int sp = 0; sp<2; sp++){
+    System.out.print(" ");
+    }
+    System.out.print("A");
+    for (int sp2 = 0; sp2<3; sp2++){
+    System.out.print("A");
+    }
+    System.out.println();//BARIS3
+    for (int sp = 0; sp<1; sp++){
+    System.out.print(" ");
+    }
+    System.out.print("A");
+    for (int sp2 = 0; sp2<7; sp2++){
+    System.out.print(" ");
+    }
+    System.out.println("A");//BARIS4
+    System.out.print("A");
+    for (int sp2 = 0; sp2<9; sp2++){
+    System.out.print(" ");
+    }
+    System.out.println("A");//BARIS5
+    }
+    
+    void c(){
+    for (int sp = 0; sp<2; sp++){
+    System.out.println("");
+    }//spasi
+    for (int c = 0; c<5; c++){
+    System.out.print("C");
+    }
+    System.out.println("");
+    for (int cb = 0; cb<3; cb++){
+        for (int cba = 0; cba<2; cba++){
+    System.out.print("C");
+    }
+    System.out.println("");
+    }
+    for (int c = 0; c<5; c++){
+    System.out.print("C");
+    }
+    System.out.println("");
+    }
+    
+    void h(){
+    int sp;
+    for (sp = 0; sp<2; sp++){
+    System.out.println("");
+    }
+    for (int h = 0; h<2; h++){
+    System.out.print("HH");
+    for (int h1 = 0; h1<4; h1++){
+    System.out.print(" ");
+    }
+    System.out.println("HH");
+    }
+    for (int h2 = 0; h2<8; h2++){
+    System.out.print("H");
+    }
+    System.out.println("");
+    for (int h = 0; h<2; h++){
+    System.out.print("HH");
+    for (int h1 = 0; h1<4; h1++){
+    System.out.print(" ");
+    }
+    System.out.println("HH");
+    }
+    }
+    
+    void y(){
+    int sp;
+    for (sp = 0; sp <2; sp++){
+    System.out.println("");
+    }
+    for (int y1 = 0; y1 < 3; y1++){
+    System.out.print("YY");
+    for(sp = 4; sp > 0; sp--){
+    System.out.print(" ");
+    }
+    System.out.println("YY");
+    }
+    System.out.println("  YYY");
+    for(int y2 = 0; y2 < 3; y2++){
+    System.out.println("  YY");
+    }
+    }
+    
+    void u(){
+    int sp;
+    for (sp = 0; sp < 2; sp++){
+        System.out.println("");
+    }
+    for(int uy = 0; uy < 4; uy++){
+        System.out.print("UU");
+        for (int ux = 0; ux <5; ux++){
+            System.out.print(" ");
+        }
+        System.out.println("UU");
+    }
+    System.out.print(" ");
+    for (int u = 0; u < 5; u++){
+    System.out.print("U");
+    }
+    System.out.println("");
+    }
+    
+    void m(){
+    int sp;
+    for(sp = 0; sp < 2; sp++){
+    System.out.println("");
+    }
+    for(int m = 0; m < 3; m++){
+    System.out.print("M");
+    }
+    for(int msp = 0; msp < 7; msp++){
+    System.out.print(" ");
+    }
+    for(int m = 0; m < 3; m++){
+    System.out.print("M");
+    }
+    System.out.println("");
+    int m1 = 3, m2 = 0, m3 = 0, m4;
+    for(int m = 0; m < 4; m++){
+    System.out.print("M");
+    }
+    for(int msp = 0; msp < 5; msp++){
+    System.out.print(" ");
+    }
+    for(int m = 0; m < 4; m++){
+    System.out.print("M");
+    }
+    System.out.println("");
+    System.out.print("MM");
+    for (m2 = m2; m2 < 1; m2++){System.out.print(" ");}
+    System.out.print("MM");
+    for (m1 = m1; m1 < 0; m1--){
+    System.out.print(" ");
+    }
+    System.out.print("MM");
+    for (m3 = m3; m3 < 1; m3++){
+    System.out.print(" ");
+    }
+    System.out.println("MM");
+    System.out.print("MM");
+    for (m2 = 0; m2 < 2; m2++){
+    System.out.print(" ");
+    }
+    System.out.print("MM");
+    System.out.print(" ");
+    System.out.print("MM");
+    for (m3 = 0; m3 < 2; m3++){System.out.print(" ");}
+    System.out.println("MM");
+    for(int m = 0; m < 2; m++){
+    System.out.print("M");
+    }
+    for(int msp = 0; msp < 3; msp++){
+    System.out.print(" ");
+    }
+    for(int m = 0; m < 3; m++){
+    System.out.print("M");
+    }
+    for(int msp = 0; msp < 3; msp++){
+    System.out.print(" ");
+    }
+    for(int m = 0; m < 2; m++){
+    System.out.print("M");
+    }
+    System.out.println("");
     }
 }
